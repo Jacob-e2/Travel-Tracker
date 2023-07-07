e0c2614b88f8a18f07a1925076c781e3c3a17705
Jacob-e2 <jacobevans04@outlook.com>
2023-07-07 23:52:27 +0100

    Added create.php and added data cleaning

--- create.php
+++ create.php
@@ -15,14 +15,18 @@
         </div>
 
         <div class="Content">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" id="Login_Form">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" id="Create_Form">
+                
+                <label for="name"><b>Name</b></label>
+                <input type="text" id="name" name="name" required>
+            
                 <label for="email"><b>Email Address</b></label>
                 <input type="email" id="email" name="email" required>
 
                 <label for="password"><b>Password</b></label>
                 <input type="password" id="password" name="password" required>
 
-                <button type="submit" id="Login_Button">Login</button>
+                <button type="submit" id="Create_Button">Login</button>
 
             </form>
         </div>
@@ -32,12 +36,15 @@
     <script src="script.js"></script>
 
     <?php
+    $name = "";
     $email = "";
     $password = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = clean($_POST["email"]);
         $password = clean($_POST["password"]);
+        $name = clean($_POST["name"]);
+
     }
 
     function clean($data) {
